99f0a2276be8ea1adde4809aca9c1edc9bb12889
Omar Martinez <fmartinez@devbot.com>
2024-04-05 15:18:55 -0600

    Adding api todos

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,11 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
+import { TodosController } from './todos/controller';
 
-interface Options {
+interface Options
+{
     port: number,
+    routes: Router,
     public_path: string
 }
 
@@ -11,25 +14,36 @@ export class Server
     private app = express();
     private readonly port: number;
     private readonly public_path: string;
+    private readonly routes: Router;
 
-    constructor(options: Options) {
-        this.port = options.port,
-        this.public_path = options.public_path
+    constructor(options: Options)
+    {
+        this.port = options.port;
+        this.public_path = options.public_path;
+        this.routes = options.routes;
     }
 
     async start()
     {
+        //* Middlewares
+        this.app.use(express.json()); // raw
+        this.app.use(express.urlencoded({ extended: true })); // x-www-form-urlencoded
 
-
+        //* Public Folder
         this.app.use(express.static(this.public_path));
 
-        this.app.get('*', (req, res) => {
-            const indexPath = path.join(__dirname + `../../../${this.public_path}/index.html`)
+        //* Routes
+        this.app.use(this.routes);
+
+        this.app.get('*', (req, res) =>
+        {
+            const indexPath = path.join(__dirname + `../../../${ this.public_path }/index.html`)
             res.sendFile(indexPath)
         })
 
-        this.app.listen(this.port, () => {
-            console.log(`Server running on port ${this.port}`);
+        this.app.listen(this.port, () =>
+        {
+            console.log(`Server running on port ${ this.port }`);
         })
     }
 }
