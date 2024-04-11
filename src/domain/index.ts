0072745b978a40bd23290b1fdd6ae37248e75ece
Omar Martinez <fmartinez@devbot.com>
2024-04-11 15:39:38 -0600

    Adding authentic and authorization

---
+++ src/domain/index.ts
@@ -0,0 +1,4 @@
+export * from './erros/custom.error';
+export * from './erros/dtos/auth/login-user.model';
+export * from './erros/dtos/auth/register-user.model';
+export * from './erros/entities/user.entity';
